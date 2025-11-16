aef10e91309b13431f3b16702661d60e796c5a48
Otieno Lazaro <otienolazaro111@gmail.com>
2025-11-16 15:45:30 +0300

    Fixing router

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,10 +1,10 @@
-import { Routes, Route } from 'react-router-dom';
+// 1. Import BrowserRouter, Routes, and Route
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeList from './components/RecipeList';
-import RecipeDetails from './components/RecipeDetails'; // We will create this
+import RecipeDetails from './components/RecipeDetails';
 import './App.css';
 
-// 1. Create a "Home" layout component
 const Home = () => (
   <>
     <AddRecipeForm />
@@ -15,14 +15,17 @@ const Home = () => (
 
 function App() {
   return (
-    <div className="App">
-      <h1>Recipe Sharing Application</h1>
-      {/* 2. Define your routes */}
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+    // 2. Wrap your entire app content in <BrowserRouter>
+    <BrowserRouter>
+      <div className="App">
+        <h1>Recipe Sharing Application</h1>
+        {/* 3. The <Routes> component now lives inside BrowserRouter */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- recipe-sharing-app/src/main.jsx
+++ recipe-sharing-app/src/main.jsx
@@ -1,16 +1,12 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// 1. Import BrowserRouter
-import { BrowserRouter } from 'react-router-dom';
+// 1. Remove the BrowserRouter import
 import App from './App.jsx';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* 2. Wrap your App component */}
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    {/* 2. Remove the <BrowserRouter> wrapper from here */}
+    <App />
   </React.StrictMode>
 );
