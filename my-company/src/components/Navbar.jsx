664a091caa10dbe7800673e3cd57dac3d6daade7
Otieno Lazaro <otienolazaro111@gmail.com>
2025-11-16 09:08:38 +0300

    Styling navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -1,11 +1,12 @@
+
 import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ padding: "10px", background: "#eee" }}>
-      <Link to="/" style={{ marginRight: "10px" }}>Home</Link>
-      <Link to="/about" style={{ marginRight: "10px" }}>About</Link>
-      <Link to="/services" style={{ marginRight: "10px" }}>Services</Link>
+    <nav style={{ backgroundColor: "#333", display: "flex", justifyContent: "space-around" }}>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/services">Services</Link>
       <Link to="/contact">Contact</Link>
     </nav>
   );
