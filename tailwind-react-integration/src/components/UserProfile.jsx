1fd93a47f9622924eabc18de317ac75140d2279e
Otieno Lazaro <otienolazaro111@gmail.com>
2025-11-28 14:08:08 +0300

    Making UserProfile more complexed and responsive error fix

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -4,28 +4,27 @@ function UserProfile() {
   return (
     
     <div className="bg-gray-100 mx-auto my-20 rounded-lg shadow-lg text-center 
-                    p-4 max-w-xs 
+                    sm:p-4 sm:max-w-xs 
                     md:p-8 md:max-w-sm">
-      
       
       <img 
         src="https://via.placeholder.com/150" 
         alt="User" 
         className="rounded-full mx-auto 
-                   w-24 h-24 
+                   sm:w-24 sm:h-24 
                    md:w-36 md:h-36"
       />
       
-     
+      
       <h1 className="text-blue-800 my-4 
-                     text-lg 
+                     sm:text-lg 
                      md:text-xl">
         John Doe
       </h1>
       
      
       <p className="text-gray-600 
-                    text-sm 
+                    sm:text-sm 
                     md:text-base">
         Developer at Example Co. Loves to write code and explore new technologies.
       </p>
